e935ae136d06231bdff4a37b66aaa70a77cc0857
Richy123rc <78038140+Richy123rc@users.noreply.github.com>
2024-03-03 21:46:19 -0600

    Add files via upload

---
+++ Venta de Bicicletas/js/bicicletas.js
@@ -0,0 +1,59 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "TREK MARLIN 7 GEN 3 2023",
+    precio: 1528.99,
+  },
+  {
+    id:2,
+    nombre: "TREK MARLIN 6 GEN 3 2023",
+    precio: 1260.00,
+  },
+  {
+    id:3,
+    nombre: "TREK FUEL EX 8 2023",
+    precio: 5698.99,
+  },
+  {
+    id:4,
+    nombre: "TREK ROSCOE 8 2023",
+    precio: 3479.99,
+  },
+  {
+    id:5,
+    nombre: "TREK X-CALIBER 9 2023",
+    precio: 2798.99,
+  },
+  {
+    id:6,
+    nombre: "TREK SLASH 8 2023",
+    precio: 6133.49,
+  }
+  ,
+  {
+    id:7,
+    nombre: "TREK DOMANE AL 2 DISC 2022",
+    precio: 1638.49,
+  }
+  ,
+  {
+    id:8,
+    nombre: "TREK DOMANE AL 3 DISC 2022",
+    precio: 1957.49,
+  }
+  ,
+  {
+    id:9,
+    nombre: "TREK X-CALIBER 8 2023",
+    precio: 2464.99,
+  }
+  ,
+  {
+    id:10,
+    nombre: "TREK PRECALIBER 24 8V NIÑO",
+    precio: 637.99,
+  }
+]
+
+ 
